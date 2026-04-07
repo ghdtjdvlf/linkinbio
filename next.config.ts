7eaf3a549382325e220e13e84828da46e6d858c7
ghdtjdvlf <tjdvlf0416@gmail.com>
2026-04-07 09:27:32 +0900

    1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.pexels.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "**" }, // 어드민에서 임의 URL 입력 허용
-    ],
+    unoptimized: true,
   },
 };
 
